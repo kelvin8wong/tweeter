99f0e65e9add498c320a4f252e9978cd0fc42931
kelvin8wong <kelvin8wong@gmail.com>
2017-10-18 00:51:52 +0000

    implemented the character counter

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,9 @@
+$(function(){
+    $('.new-tweet textarea' ).on('input', function() {
+        let length = $(this).val().length;
+        $(this).parent().find('.counter').text(140-length);
+        if (length > 140) {
+            $(this).parent().find('.counter').css('color', 'red');
+        };
+    }); 
+});
