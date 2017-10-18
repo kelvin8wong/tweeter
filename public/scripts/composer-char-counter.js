19f0f3b8b5b7e515e6e24d6f06787eab5826ed09
Kelvin Wong <kelvin8wong@gmail.com>
2017-10-18 01:00:53 -0700

    fixed some styling for new-tweet container

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -4,6 +4,8 @@ $(function(){
         $(this).parent().find('.counter').text(140-length);
         if (length > 140) {
             $(this).parent().find('.counter').css('color', 'red');
-        };
+        } else {
+            $(this).parent().find('.counter').css('color','black');
+        }
     }); 
 });
